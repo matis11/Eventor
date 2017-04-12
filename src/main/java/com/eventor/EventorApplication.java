5e9b86fb9e5a9673cc1cb5890c1d80426ea135f1
Paweł Szudrowicz <pablo.szudrowicz@gmail.com>
2017-04-12 18:40:42 +0200

    New start

---
+++ src/main/java/com/eventor/EventorApplication.java
@@ -0,0 +1,32 @@
+package com.eventor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+//
+@SpringBootApplication
+public class EventorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventorApplication.class, args);
+	}
+
+}
+//
+//@ComponentScan
+//@EnableAutoConfiguration
+//public class EventorApplication extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(EventorApplication.class);
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(EventorApplication.class, args);
+//	}
+//}
