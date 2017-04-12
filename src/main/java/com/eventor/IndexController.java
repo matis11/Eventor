4b36e7d00d0f607a61a6f2b248423b4cd1f83c14
Paweł Szudrowicz <pablo.szudrowicz@gmail.com>
2017-04-12 22:45:47 +0200

    Add standard comments for javadoc

--- src/main/java/com/eventor/IndexController.java
+++ src/main/java/com/eventor/IndexController.java
@@ -8,14 +8,25 @@ import java.sql.DriverManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ * A class for getting the index.html and manage SQL database connection.
+ */
 @Controller
 public class IndexController {
 
+    /**
+     * Function for displaying .html content.
+     * @return String containing the name of .html file to display.
+     */
     @RequestMapping("/")
     public String index() {
         return "index";
     }
 
+    /**
+     *  Function responsible for connection to SQL Database
+     */
     public void connectToDB() {
         String url = "jdbc:sqlserver://eventor.database.windows.net:1433;database=EventorDB;user=pablo;password=Eventor123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";//String.format("jdbc:sqlserver://%s.database.windows.net:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
         Connection connection = null;
@@ -33,7 +44,7 @@ public class IndexController {
 
             try (Statement statement = connection.createStatement();
                  ResultSet resultSet = statement.executeQuery(selectSql)) {
-                
+
                 while (resultSet.next())
                 {
                     System.out.println(resultSet.getString(1) + " "
