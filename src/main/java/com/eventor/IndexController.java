c86076242d1d9c5d3aa492f2fce2bf9f81b1909e
Paweł Szudrowicz <pablo.szudrowicz@gmail.com>
2017-04-12 22:38:10 +0200

    Database connection using JDBC

--- src/main/java/com/eventor/IndexController.java
+++ src/main/java/com/eventor/IndexController.java
@@ -1,5 +1,10 @@
 package com.eventor;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +14,36 @@ public class IndexController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    public void connectToDB() {
+        String url = "jdbc:sqlserver://eventor.database.windows.net:1433;database=EventorDB;user=pablo;password=Eventor123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";//String.format("jdbc:sqlserver://%s.database.windows.net:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+
+            System.out.println("Query data example:");
+            System.out.println("=========================================");
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT * FROM eventor.users";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSql)) {
+                
+                while (resultSet.next())
+                {
+                    System.out.println(resultSet.getString(1) + " "
+                            + resultSet.getString(2));
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
