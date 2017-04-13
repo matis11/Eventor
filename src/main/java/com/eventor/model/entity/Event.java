710ddfe988859ea0dd700b5013bbf5c9ee907f97
Mateusz Bartos <geekbajt@gmail.com>
2017-04-13 04:21:14 +0200

    Added working MySQL module

---
+++ src/main/java/com/eventor/model/entity/Event.java
@@ -0,0 +1,93 @@
+package com.eventor.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Basic entity for event handling. Annotated as default Spring model class.
+ *
+ * @return Event
+ */
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer level;
+    private Integer seatsNumber;
+
+    /**
+     * Create new event
+     * @param name Event name
+     * @param description Event description
+     * @return Event
+     */
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    /**
+     * Unique event Id
+     * @return Event Id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * Event name
+     * @return Event name
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Event description
+     * @return Event description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Event level
+     * @return Event level
+     */
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    /**
+     * Seats number on event
+     * @return Seats number on event
+     */
+    public Integer getSeatsNumber() {
+        return seatsNumber;
+    }
+
+    public void setSeatsNumber(Integer seatsNumber) {
+        this.seatsNumber = seatsNumber;
+    }
+}
---
+++ src/main/java/com/eventor/model/repository/EventRepository.java
@@ -0,0 +1,15 @@
+package com.eventor.model.repository;
+
+import com.eventor.model.entity.Event;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+/**
+ * Event Repository for CRUD operations.
+ * @return CRUD wrapper for events
+ */
+public interface EventRepository extends CrudRepository<Event, Long> {
+
+}
